31187988fe6aac85204f221b59f7251d32e1e051
Ray <rayees.rahman@icahn.mssm.edu>
2019-06-19 15:35:28 -0400

    cleaned code

--- structural-signatures-2.0.sh
+++ structural-signatures-2.0.sh
@@ -12,7 +12,6 @@
 ### Environment Variables:       ###
 ####################################
 
-##for folds clean input (',',":","'', '""'")
 
 if [ -z "$homed" ]; 
 then 
@@ -30,8 +29,7 @@ else
 	printf "\n\033[31mStructure database not found, please untar the structure database in:\n\t$homed/database/\n\033[0m\n"
 	exit 
 fi  
-u="Usage:\n\t -i input gene list \n\t -t domain enrichment (domain) scop enrichment (fold) or both (both)
- \n\t -n name type: gene name (gn) or uniprot id (uid)\n\t -o output file name \n\n)";  
+u="Usage:\n\t -i input gene list \n\t -t domain enrichment (domain) scop enrichment (fold) or both (both)\n\t -n name type: gene name (gn) or uniprot id (uid)\n\t -o output file name \n\n)";  
 while getopts ":i:o:t:n:d:b:f:g:T:p:lh:e:P:y:c:zh:" opt; do
     case $opt in
 		i) 	input=$OPTARG ;;  #input
@@ -55,6 +53,7 @@ numgenes=$( cat ./$output.found.genes )
 if [ $type == "domain" ] 
 then
     Rscript $homed/bin/scripts/compute_representation.R $output.domain.cnt $homed/bin/files/backgrounds/default.background.ipr.domain.cnt $numgenes $output domain  #2> /dev/null
+	# remove files ----------------------------------------
 	rm $output.domain.cnt
 	rm $output.found.genes
 elif [ $type == "fold" ]
@@ -62,34 +61,36 @@ then
 
 	cat ./$output.scop.fold.cnt | cut -f1,2 -d"," > $output.tmp.fold 
 	Rscript $homed/bin/scripts/compute_representation.R $output.tmp.fold $homed/bin/files/backgrounds/default.background.scop.fold.cnt.2 $numgenes $output fold #2> /dev/null
-	rm ./$output.tmp.fold
-	rm ./$output.scop.fold.cnt 
 	cat ./$output.scop.superfam.cnt | cut -f1,2 -d"," > $output.tmp.superfam
 	Rscript $homed/bin/scripts/compute_representation.R $output.tmp.superfam $homed/bin/files/backgrounds/default.background.scop.superfam.cnt.2 $numgenes $output superfam  #2> /dev/null
-	rm ./$output.tmp.superfam 
-	rm ./$output.scop.superfam.cnt
 	cat ./$output.scop.family.cnt | cut -f1,2 -d"," > $output.tmp.fam
 	Rscript $homed/bin/scripts/compute_representation.R $output.tmp.fam $homed/bin/files/backgrounds/default.background.scop.family.cnt.2 $numgenes $output family  #2> /dev/null
+	# remove files ----------------------------------------
+	rm ./$output.tmp.fold
+	rm ./$output.scop.fold.cnt 
+	rm ./$output.tmp.superfam 
+	rm ./$output.scop.superfam.cnt
 	rm ./$output.tmp.fam
 	rm ./$output.scop.family.cnt
 	rm $output.found.genes
 	rm ./$output.scop.class.cnt
 else 
 	Rscript $homed/bin/scripts/compute_representation.R $output.domain.cnt $homed/bin/files/backgrounds/default.background.ipr.domain.cnt $numgenes $output domain  #2> /dev/null
-	rm $output.domain.cnt
 	cat ./$output.scop.fold.cnt | cut -f1,2 -d"," > $output.tmp.fold 
 	Rscript $homed/bin/scripts/compute_representation.R $output.tmp.fold $homed/bin/files/backgrounds/default.background.scop.fold.cnt.2 $numgenes $output fold  #2> /dev/null
-	rm ./$output.tmp.fold
-	rm ./$output.scop.fold.cnt
 	cat ./$output.scop.superfam.cnt | cut -f1,2 -d"," > $output.tmp.superfam
 	Rscript $homed/bin/scripts/compute_representation.R $output.tmp.superfam $homed/bin/files/backgrounds/default.background.scop.superfam.cnt.2 $numgenes $output superfam  #2> /dev/null
-	rm ./$output.tmp.superfam 
-	rm ./$output.scop.superfam.cnt 
 	cat ./$output.scop.family.cnt | cut -f1,2 -d"," > $output.tmp.fam
 	Rscript $homed/bin/scripts/compute_representation.R $output.tmp.fam $homed/bin/files/backgrounds/default.background.scop.family.cnt.2 $numgenes $output family #2> /dev/null
+	# remove files ----------------------------------------
+	rm ./$output.domain.cnt
+	rm ./$output.tmp.fold
+	rm ./$output.scop.fold.cnt
+	rm ./$output.tmp.superfam 
+	rm ./$output.scop.superfam.cnt 
 	rm ./$output.tmp.fam
 	rm ./$output.scop.family.cnt
-	rm $output.found.genes
+	rm ./$output.found.genes
 	rm ./$output.scop.class.cnt
 fi
 exit ; 
